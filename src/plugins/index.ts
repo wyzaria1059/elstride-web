d23632b087906daf1e8b81454cb771f9d350d00d
Wyzaria Viozta <wyzaria.viozta@gmail.com>
2025-04-07 01:32:35 +0700

    Add element plus as main ui library

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -1,8 +1,10 @@
 import type { App } from 'vue'
 import { pinia } from './pinia'
 import router from '@/router'
+import ElementPlus from './element-plus'
 
 export function registerPlugins(app: App<Element>) {
   app.use(pinia)
   app.use(router)
+  app.use(ElementPlus)
 }
